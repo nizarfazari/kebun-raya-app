580bbee4db7712a06873e8bd40ad51f79319c700
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-02-28 14:22:06 +0700

    validation and alert in register login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -6,6 +6,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
+import AlertChakra from '~/components/alert-chakra';
 
 
 interface ILoginProps {
@@ -55,20 +56,20 @@ const Login: React.FunctionComponent<ILoginProps> = (props) => {
                             menjadi lebih mudah
                         </h2>
                         <form action="" className='flex flex-col gap-4 max-w-[500px] mx-auto' onSubmit={handleSubmit(onSubmit)}>
-                            <FormControl className='h-[90px]'>
+                        {Object.keys(errors).length > 0 && (
+                            <AlertChakra description='Terdapat masalah pada form' />
+                        )}
+                            <FormControl className='h-[80px]'>
                                 <FormLabel htmlFor='email'>Email address</FormLabel>
-                                <Input type='email' id='email' className='!bg-slate-100 !border !border-slate-600 focus-visible:!border-primary-500 focus-visible:!shadow-none' {...register('email', {
-                                    required: 'This is required',
-                                    minLength: { value: 4, message: 'Minimum length should be 4' },
-                                })} />
+                                <Input type='email' id='email' className='!bg-slate-100 !border !border-slate-600 focus-visible:!border-primary-500 focus-visible:!shadow-none' {...register('email')} />
                                 <p className='text-rose-600'>
                                     {errors.email && errors.email.message}
                                 </p>
                             </FormControl>
-                            <FormControl className='h-[90px]'>
+                            <FormControl className='h-[80px]'>
                                 <FormLabel htmlFor='password'>Password</FormLabel>
                                 <InputGroup >
-                                    <Input type={show ? 'text' : 'password'} id='password' placeholder='Enter password' className='!bg-slate-100 !border !border-slate-600' {...register("password")} />
+                                    <Input type={show ? 'text' : 'password'} id='password' placeholder='Enter password' className='!bg-slate-100 !border !border-slate-600 focus-visible:!border-primary-500 focus-visible:!shadow-none' {...register("password")} />
                                     <InputRightElement width='4.5rem'>
                                         <div onClick={handleClick}>
                                             {show ? <AiFillEye /> : <AiFillEyeInvisible />}
@@ -80,7 +81,7 @@ const Login: React.FunctionComponent<ILoginProps> = (props) => {
                                 </p>
                             </FormControl>
 
-                            <button type="submit" className='w-full py-2 rounded-lg bg-primary-700 text-white mt-2'>Login</button>
+                            <button type="submit" className='w-full py-2 rounded-lg bg-primary-700 text-white mt-7'>Login</button>
                             <p className='text-center font-normal text-sm'>Apakah anda sudah punya akun? <Link href={'/auth/register'} className='text-primary-700 font-semibold'>Register</Link></p>
                         </form>
                     </div>
